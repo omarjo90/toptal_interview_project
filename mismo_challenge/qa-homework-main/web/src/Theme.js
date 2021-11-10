047bc581f744ac21995b885c36762c273cb7f9f1
Omar Guzman <omarjo90@gmail.com>
2021-11-10 14:19:23 -0600

    Delete mismo_challenge directory

--- mismo_challenge/qa-homework-main/web/src/Theme.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "./tailwind.config.js";
-
-const { theme } = resolveConfig(tailwindConfig);
-
-export const Theme = (props) => <ThemeProvider {...props} {...{ theme }} />;
--- mismo_challenge/qa-homework-main/web/src/components/Main.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "twin.macro";
-
-export const Main = ({ children }) => {
-  return <main tw="w-full h-full flex-auto bg-gray-600">{children}</main>;
-};
